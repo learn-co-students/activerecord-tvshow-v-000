8393a7e4671add70101f1b842128af599b9055b6
onyoo <roberto9m9@gmail.com>
2015-12-09 14:46:03 +0100

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -4,7 +4,7 @@ class Show <ActiveRecord::Base
  end
 
  def self.most_popular_show
-   self.find_by rating: highest_rating
+   self.where("rating = ?", highest_rating).first
  end
 
  def self.lowest_rating
@@ -12,7 +12,7 @@ class Show <ActiveRecord::Base
  end
 
  def self.least_popular_show
-   self.find_by rating: lowest_rating
+   self.where("rating = ?", lowest_rating).first
  end
 
  def self.ratings_sum
@@ -20,7 +20,7 @@ class Show <ActiveRecord::Base
  end
 
  def self.popular_shows
-   self.where("rating > 5")
+   self.where("rating > ?", 5)
  end
  
  def self.shows_by_alphabetical_order
