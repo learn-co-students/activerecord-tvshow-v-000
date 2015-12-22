fb19d1739883882f40f7cc64204be09f7dad2bfe
Shana L Moore <shana.lavina.moore@gmail.com>
2015-12-21 18:52:43 -0800

    Done.

---
+++ app/models/show.rb
@@ -0,0 +1,30 @@
+class Show < ActiveRecord::Base
+
+  def highest_rating
+    self.maximum(:rating)
+  end
+
+  def most_popular_show
+    self.where("rating = ?", self.highest_rating).first
+  end
+
+  def lowest_rating
+    self.minimum(:rating)
+  end
+
+  def least_popular_show
+    self.where("rating = ?", self.lowest_rating).first
+  end
+
+  def ratings_sum
+    self.sum(:rating)
+  end
+
+  def popular_shows
+    self.where("rating > ?", 5)
+  end
+
+  def shows_by_alphabetical_order
+    self.order(:name)
+  end
+end
