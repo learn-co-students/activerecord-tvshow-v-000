ce250bf2bfd57fa63eeb7b583ad447af67ca8bb3
glee38 <glee3388@gmail.com>
2016-03-24 18:57:44 -0400

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -5,6 +5,10 @@ class Show < ActiveRecord::Base
 
   def self.most_popular_show
     Show.where("rating = #{highest_rating}").first
+    # can also write:
+    # Show.where(rating: highest_rating).first
+    # or
+    # order("rating DESC").first
   end
 
   def self.lowest_rating
@@ -13,10 +17,16 @@ class Show < ActiveRecord::Base
 
   def self.least_popular_show
     Show.where("rating = #{lowest_rating}").first
+    # can also write:
+    # Show.where(rating: lowest_rating).first
+    # or
+    # order("rating ASC").first
   end
 
   def self.ratings_sum
     Show.sum(:rating)
+    # or
+    # sum("rating")
   end
 
   def self.popular_shows
@@ -25,5 +35,7 @@ class Show < ActiveRecord::Base
 
   def self.shows_by_alphabetical_order
     Show.order("name ASC")
+    # can also write:
+    # Show.order(name: :asc)
   end
 end
