83f9000308530b878a54459a1384a497cfe0225f
Heber Sandoval <he3ers@gmail.com>
2016-04-07 13:05:56 -0400

    Create a table using ActiveRecord and querying methods

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,9 +1,29 @@
 class Show < ActiveRecord::Base
-  def highest_rating
+  def self.highest_rating
     Show.maximum(:rating)
   end
 
-  def most_popular_show
-    #Show.where("rating > ?" highest_rating)
+  def self.most_popular_show
+    Show.find_by(rating: highest_rating)
+  end
+
+  def self.lowest_rating
+    Show.minimum(:rating)
+  end
+
+  def self.least_popular_show
+    Show.find_by(rating: lowest_rating)
+  end
+
+  def self.ratings_sum
+    Show.sum(:rating)
+  end
+
+  def self.popular_shows
+    Show.where("rating > ?", 5)
+  end
+
+  def self.shows_by_alphabetical_order
+    Show.order(name: :asc)
   end
 end
