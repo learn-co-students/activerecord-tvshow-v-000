83efd00275b3b255b400d2b7aff8f6e80d23110a
cassaram09 <cassaram09@gmail.com>
2016-05-10 15:34:47 -0400

    Done.

---
+++ app/models/show.rb
@@ -0,0 +1,29 @@
+require 'pry'
+
+class Show < ActiveRecord::Base
+  binding.pry 
+  
+  def highest_rating
+    Show.maximum("rating")
+  end
+
+  def most_popular_show
+
+  end
+
+  def lowest_rating
+  end
+
+  def least_popular_show
+  end
+
+  def ratings_sum
+  end
+
+  def popular_shows
+  end
+
+  def shows_by_alphabetical_order
+  end
+
+end
