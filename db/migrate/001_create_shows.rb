e8271a49de2a731ed172ae152f2728f2e4731425
Shmuwol <shmullywolfson@gmail.com>
2016-06-17 02:37:26 -0400

    Done.

--- db/migrate/001_create_shows.rb
+++ db/migrate/001_create_shows.rb
@@ -1,6 +1,6 @@
 class CreateShows < ActiveRecord::Migration
   def change
-    creat_table :shows do |t|
+    create_table :shows do |t|
       t.string :name
       t.string :network
       t.string :day
