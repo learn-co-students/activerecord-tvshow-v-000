e4ba7d3e316e2c46a8b88331a7274b3d29288bd2
Jonathan Stone <jon_p_stone@yahoo.com>
2016-07-15 08:15:18 +0000

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,29 @@
-class CreateShows < ActiveRecord::Base
+class Show < ActiveRecord::Base
+  def highest_rating
+    self.maximum(:rating)
+  end
 
+  def most_popular_show
+    self.highest_rating(:name)
+  end
+
+  def lowest_rating
+    self.minimum(:rating)
+  end
+
+  def least_popular_show
+    self.lowest_rating(:show)
+  end
+
+  def ratings_sum
+    self.sum(:ratings)
+  end
+
+  def popular_shows
+    self.where(:rating > 5)
+  end
+
+  def shows_by_alphabetical_order
+    self.order(:name)
+  end
 end
