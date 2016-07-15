6f996f2e943b8c560244106fb8e4423cdcb7644f
Jonathan Stone <jon_p_stone@yahoo.com>
2016-07-15 08:23:45 +0000

    Done.

---
+++ app/show_controller.rb
@@ -0,0 +1,27 @@
+def highest_rating
+  self.maximum(:rating)
+end
+
+def most_popular_show
+  self.highest_rating(:name)
+end
+
+def lowest_rating
+  self.minimum(:rating)
+end
+
+def least_popular_show
+  self.lowest_rating(:show)
+end
+
+def ratings_sum
+  self.sum(:ratings)
+end
+
+def popular_shows
+  self.where(:rating > 5)
+end
+
+def shows_by_alphabetical_order
+  self.order(:name)
+end
