7763cc2f48628cf9531af913957040cdbcfabe3b
Baruch Steinmetz <boruchsteinmetz@outlook.com>
2016-10-09 21:09:12 +0000

    Done.

---
+++ app/models/show.rb
@@ -0,0 +1,18 @@
+class Show < ActiveRecord::Base
+  def self.highest_rating
+    Show.maximum(:rating)
+  end
+
+  def self.most_popular_show
+    Show.where(rating = highest_rating).take
+  end
+
+  def self.lowest_rating
+    Show.minimum(:rating)
+  end
+
+  def self.least_popular_show
+    Show.where("rating = #{lowest_rating}").take
+  end
+
+end
