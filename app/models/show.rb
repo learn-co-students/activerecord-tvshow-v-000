afe9bbbe18d92ddccf0d5a919fe060cff8d38310
Matthew Hallquist <matthallquist@yahoo.com>
2016-12-08 20:54:18 +0000

    Done.

---
+++ app/models/show.rb
@@ -0,0 +1,30 @@
+class Show < ActiveRecord::Base
+  def self.highest_rating
+    show = Show.maximum(:rating)
+  end
+
+  def self.lowest_rating
+    show = Show.minimum(:rating)
+  end
+
+  def self.most_popular_show
+    show = Show.order(:rating).last
+  end
+
+  def self.least_popular_show
+    show = Show.order(:rating).first
+  end
+
+  def self.ratings_sum
+    sum = Show.sum(:rating)
+  end
+
+  def self.popular_shows
+    shows = Show.where("rating > ?", 5)
+  end
+
+  def self.shows_by_alphabetical_order
+    shows = Show.order(name: :asc)
+    shows = shows.flatten
+  end
+end
