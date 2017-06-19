ef0abb6b73a39dfe806b45b6801d07d889cb8fdd
Lia HUANG <lisa.yc.huang@gmail.com>
2017-06-19 13:05:45 +0000

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,13 +1,12 @@
 require "pry"
 
 class Show < ActiveRecord::Base
-  def self.highest_ratings
-    binding.pry
+
+  def self.highest_rating
     Show.maximum(:rating)
   end
 
   def self.most_popular_show
 
   end
-
 end
