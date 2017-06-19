b9fb643f7aa016d1450e0e1765aaf463f710be6f
Lia HUANG <lisa.yc.huang@gmail.com>
2017-06-19 13:58:50 +0000

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -7,6 +7,28 @@ class Show < ActiveRecord::Base
   end
 
   def self.most_popular_show
+    rating_num = self.highest_rating
+    Show.find_by(rating: rating_num)
+  end
 
+  def self.lowest_rating
+    Show.minimum(:rating)
+  end
+
+  def self.least_popular_show
+    rating_num = self.lowest_rating
+    Show.find_by(rating: rating_num)
+  end
+
+  def self.ratings_sum
+    Show.sum(:rating)
+  end
+
+  def self.popular_shows
+    Show.where("rating > '5'")
+  end
+
+  def self.shows_by_alphabetical_order
+    Show.order(:name)
   end
 end
