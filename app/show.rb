084d38625e9948abd2e1e98ba8a4d0a9bcc10790
charlie lee <ctlee9096@gmail.com>
2017-07-01 21:09:16 +0000

    Done.

--- app/show.rb
+++
@@ -1,7 +0,0 @@
-class Show < ActiveRecord::Base
-  def self.highest_rating
-  maximum("rating")
-  end
-end
-#<class name>.sum(:<column_name>)
-#Client.maximum("age")
--- db/migrate/002_add_season_to_shows.rb
+++ db/migrate/002_add_season_to_shows.rb
@@ -1,6 +1,6 @@
 class AddSeasonToShows < ActiveRecord::Migration
   def change
-    add_column :shows, :integer, :string
+    add_column :shows, :season, :string
   end
 end
 
