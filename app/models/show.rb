ac922623a2c539ad9f8efa6dcb373a4642b9b2e2
Bhagyashri Padmawar <bhagyashri.padmawar@gmail.com>
2017-07-12 18:47:03 +0000

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,6 +1,32 @@
 class Show < ActiveRecord::Base
 
-  def highest_rating
+  def self.highest_rating
     Show.maximum("rating")
   end
+
+  def self.most_popular_show
+    Show.order("rating DESC").limit(1).take
+  end
+
+  def self.lowest_rating
+    Show.minimum("rating")
+  end
+
+  def self.least_popular_show
+    Show.order("rating").limit(1).take
+  end
+
+  def self.ratings_sum
+    Show.sum("rating")
+  end
+
+  def self.popular_shows
+    Show.where("rating > ?", 5)
+  end
+
+  def self.shows_by_alphabetical_order
+    Show.order("name")
+  end
+
+
 end
