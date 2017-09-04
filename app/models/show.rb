b549ad5dc47b7bd1a92405913266bd7626d40a44
Robert Peckham <peckham.rob@gmail.com>
2017-09-04 20:14:49 +0000

    Done.

---
+++ app/models/show.rb
@@ -0,0 +1,30 @@
+class Show < ActiveRecord::Base
+  def self.highest_rating
+    Show.maximum(:rating)
+  end
+
+  def self.most_popular_show
+    Show.order(rating: :desc).limit(1)
+  end
+
+  def self.lowest_rating
+    Show.minimum(:rating)
+  end
+
+  def least_popular_show
+
+  end
+
+  def ratings_sum
+
+  end
+
+  def popular_shows
+
+  end
+
+  def shows_by_alphabetical_order
+
+  end
+
+end
