93cdcd02d346d6624dc2be8f09862df606668eac
Elvin <Dimix181@Gmail.com>
2017-09-13 10:21:22 -0400

    all test are passing

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,37 @@
 class Show < ActiveRecord::Base
+# -------------
+# not part of the test spec
+  def self.rating_desc
+    self.order(rating: :desc)
+  end
+# ------------
+
+  def self.most_popular_show
+    self.rating_desc.limit(1).first
+  end
+
+  def self.highest_rating
+    self.most_popular_show.rating
+  end
+
+  def self.least_popular_show
+    self.rating_desc.reverse.first
+  end
+
+  def self.lowest_rating
+    self.least_popular_show.rating
+  end
+
+  def self.ratings_sum
+    self.sum(:rating)
+  end
+
+  def self.popular_shows
+    self.rating_desc.where("rating > ?", '5')
+  end
+
+  def self.shows_by_alphabetical_order
+  self.order(name: :asc)
+  end
 
 end
--- db/migrate/003_rename_fix_column_name.rb
+++ db/migrate/003_rename_fix_column_name.rb
@@ -4,4 +4,5 @@ class RenameFixColumnName < ActiveRecord::Migration[5.1]
       rename_column :shows, :seasons,:season
   end
 
+  
 end
