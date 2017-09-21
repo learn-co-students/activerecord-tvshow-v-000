56ddff52cabfb8072d61eac7b87a7125d6ce93f8
daniellecroxton <danikrist@gmail.com>
2017-09-21 09:06:23 -0500

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,31 +1,3 @@
 class Show < ActiveRecord::Base
 
-  def highest_rating
-    Show.maximum(:rating)
-  end
-
-  def most_popular_show
-    Show.maximum(:rating).name
-  end
-
-  def lowest_rating
-    Show.minimum(:rating)
-  end
-
-  def least_popular_show
-    Show.minimum(:rating).name
-  end
-
-  def ratings_sum
-    Show.sum(:rating)
-  end
-
-  def popular_shows
-    Show.where("rating > ?", 5)
-  end
-
-  def shows_by_alphabetical_order
-    Show.where(name: :asc)
-  end
-
 end
