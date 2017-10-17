3fdcb370c7a169a8a04fcbb3a0aee7c7cc08bed6
Richard Steinbach <steinbach.dick@gmail.com>
2017-10-17 08:50:27 -0700

    Advanced Finding Lab

--- Rakefile
+++ Rakefile
@@ -18,3 +18,5 @@ namespace :db do
     File.delete(connection_details.fetch('database')) if File.exist?(connection_details.fetch('database'))
   end
 end
+
+
---
+++ app/models/show.rb
@@ -0,0 +1,38 @@
+require 'pry'
+
+class Show < ActiveRecord::Base
+
+	def self.highest_rating
+		self.maximum("rating")
+	end
+
+	def self.most_popular_show
+		most_popular = self.find_by(rating: self.highest_rating)
+		
+	end
+   
+  	def self.lowest_rating
+  		self.minimum("rating")
+  	end
+    
+
+  	def self.least_popular_show
+  		least_popular = self.find_by(rating: self.lowest_rating)
+  	end
+    
+
+  	def self.ratings_sum
+
+		self.sum("rating")
+  	end
+    
+	def self.popular_shows
+		self.all.where("rating > 5")
+	end
+    
+  	def self.shows_by_alphabetical_order
+  		self.all.order(name: :asc)
+  		
+  	end
+    
+end
