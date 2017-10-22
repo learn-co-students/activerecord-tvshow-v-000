9e868304e132df08fc434f490d5c9a9d2f96d63a
John Tuttle <thejwt@gmail.com>
2017-10-22 06:50:51 +0000

    Done.

---
+++ app/models/show.rb
@@ -0,0 +1,31 @@
+class Show < ActiveRecord::Base
+
+  def self.highest_rating
+    maximum(:rating)
+  end
+
+  def self.most_popular_show
+    Show.find_by(rating: self.highest_rating)
+  end
+
+  def self.lowest_rating
+    minimum(:rating)
+  end
+
+  def self.least_popular_show
+    Show.find_by(rating: self.lowest_rating)
+  end
+
+  def self.ratings_sum
+    sum(:rating)
+  end
+
+  def self.popular_shows
+    Show.where("rating > ?",[5])
+  end
+
+  def self.shows_by_alphabetical_order
+    order(:name)
+  end
+
+end
