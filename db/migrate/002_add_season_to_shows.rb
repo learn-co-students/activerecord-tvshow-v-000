7026797dcfb7197207b302ca6e83f8130ddab250
github username <github email address>
2017-10-23 13:33:03 -0400

    Done.

---
+++ db/migrate/002_add_season_to_shows.rb
@@ -0,0 +1,6 @@
+# and write a migration to add a column, season, to the shows table. The datatype of this column is string.
+class AddSeasonToShows < ActiveRecord::Migration
+    def change
+      add_column :shows, :season, :string
+    end
+end
