1b81899c3475e5b48439ea0e786ec4eb4b6a8996
Adam Tropp <adt6261@gmail.com>
2017-11-08 19:10:52 +0000

    Done.

---
+++ app/models/show.rb
@@ -0,0 +1,29 @@
+class Show < ActiveRecord::Base
+  def highest_rating
+    self.maximum(:rating)
+  end
+
+  def most_popular_show
+    self.where(highest_rating)
+  end
+
+  def lowest_rating
+    self.minimum(:rating)
+  end
+
+  def least_popular_show
+    self.where(lowest_rating)
+  end
+
+  def ratings_sum
+    self.sum(:rating)
+  end
+
+  def popular_shows
+    self.where("rating > ?", 5)
+  end
+
+  def shows_by_alphabetical_order
+    self.order(name: :asc)
+  end
+end
