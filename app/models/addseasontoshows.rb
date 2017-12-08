45ed48949f537c97998bc3bf57f6da05b8537991
Miriam Peskowitz <miriam5000@gmail.com>
2017-12-08 01:32:22 +0000

    Done.

--- app/models/addseasontoshows.rb
+++
@@ -1,2 +0,0 @@
-class AddSeason < ActiveRecord::Base
-end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,2 +1,35 @@
+require 'pry'
+
 class Show < ActiveRecord::Base
+  attr_accessor :name
+
+  def self.highest_rating
+    Show.maximum("rating")
+  end
+
+  def self.most_popular_show
+    Show.name.maximum("rating")
+  end
+
+  def self.lowest_rating
+    self.minimum("rating")
+  end
+
+  def self.least_popular_show
+    Show.name.minimum(rating)
+  end
+
+  def self.ratings_sum
+    Shows.sum("rating")
+  end
+
+  def self.popular_shows
+    popular = []
+    popular = Show.all.where("rating > ?", 5)
+  end
+
+  def self.shows_by_alphabetical_order
+    alpha = []
+    alpha = Show.name.order("ASC")
+  end
 end
