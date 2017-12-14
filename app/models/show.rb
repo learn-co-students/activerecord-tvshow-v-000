3d96899516491858d1494a6e5fa08a8f3c4791e6
Caroline Shields <caroline.shields@amkor.com>
2017-12-14 18:47:20 +0000

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -5,7 +5,8 @@ class Show < ActiveRecord::Base
   end
 
   def self.most_popular_show
-    Show.find_by(highest_rating)
+    high_rating = Show.highest_rating
+    Show.where("rating = ?",high_rating)[0]
   end
 
   def self.lowest_rating
@@ -13,13 +14,21 @@ class Show < ActiveRecord::Base
   end
 
   def self.least_popular_show
-    # Show.find_by(lowest_rating)
-     binding.pry
+    low_rating = Show.lowest_rating
+    Show.where("rating = ?",low_rating)[0]
 
   end
 
   def self.ratings_sum
-    Show.sum(:ratings)
+    Show.sum(:rating)
+  end
+
+  def self.popular_shows
+    Show.where("rating > ?",5)
+  end
+
+  def self.shows_by_alphabetical_order
+    Show.order(name: :asc)
   end
 
 end #ends show class
