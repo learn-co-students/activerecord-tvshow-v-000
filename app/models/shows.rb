0f5408cad76e01a6990f9106dc66d63605a43a9a
Kaitlin Kelly <kk504408@gmail.com>
2017-12-20 03:56:27 +0000

    Done.

---
+++ app/models/shows.rb
@@ -0,0 +1,33 @@
+class Show < ActiveRecord::Base
+
+  def self.highest_rating
+    Show.maximum(:rating)
+  end
+
+  def self.most_popular_show
+    r = Show.highest_rating
+    Show.where("rating = #{r}").first
+  end
+
+  def self.lowest_rating
+    Show.minimum(:rating)
+  end
+
+  def self.least_popular_show
+    r = Show.lowest_rating
+    Show.where("rating = #{r}").first
+  end
+
+  def self.ratings_sum
+    Show.sum(:rating)
+  end
+
+  def self.popular_shows
+    Show.where("rating > 5")
+  end
+
+  def self.shows_by_alphabetical_order
+    Show.order("name ASC")
+  end
+
+end
