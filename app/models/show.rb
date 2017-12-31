e91576ab9ccc02e3c74a191652c91e127dcc5299
github username <github email address>
2017-12-31 17:15:47 -0600

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,43 +1,43 @@
 class Show < ActiveRecord::Base
 
-    def highest_rating
-        Show.rating.maximum("rating")
+    def self.highest_rating
+        Show.maximum(:rating) #if can add "of them all" to the end of the statement then it's a class method ... invovles not just once instance)
     end
     
     # : this method should return the highest value in the ratings column. hint: if there is a #minimum Active Record method, might there be a #maximum method?
 
-    def most_popular_show
-        Show.name.highest_rating
+    def self.most_popular_show
+        Show.where('rating = ?', Show.highest_rating)[0] #wasn't able to use flatten
     end
 
     # : this method should return the show with the highest rating. hint: use the #highest_rating method as a helper method.
 
-    def lowest_rating
-        Show.minimum("rating")
+    def self.lowest_rating
+        Show.minimum(:rating)
     end
     
     # : returns the lowest value in the ratings column.
     
-    def least_popular_show
-        Show.name.lowest_rating
+    def self.least_popular_show
+        Show.where('rating = ?', Show.lowest_rating)[0] #wasn't able to use flatten
     end
 
         # : returns the show with the lowest rating.
 
-    def ratings_sum
+    def self.ratings_sum
         Show.sum("rating")
     end
 
         
         # returns the sum of all of the ratings.
-    def popular_shows
+    def self.popular_shows
         Show.where("rating > 5")
     end
     
     # returns an array of all of the shows that have a rating greater than 5. hint: use the #where Active Record method.
 
-    def shows_by_alphabetical_order
-        Show.order(name: :desc)
+    def self.shows_by_alphabetical_order
+        Show.order(:asc)
     end
     
     # : returns an array of all of the shows sorted by alphabetical order according to their names. hint: use the #order Active Record method.
