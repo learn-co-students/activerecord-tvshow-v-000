765cf88b9e8e1ae6a87ff9660c872e599f9dea2f
Daniel Karagiannis <dan.karagiannis@protonmail.ch>
2018-01-02 20:59:07 +0000

    Done.

--- db/migrate/001_create_shows.rb
+++ db/migrate/001_create_shows.rb
@@ -1,7 +1,7 @@
-class CreateShows ActiveRecord::Migration[5.1]
+class CreateShows < ActiveRecord::Migration[4.2]
     def change 
         create_table :shows do |t|
-            t.string :name 
+            t.string :name
             t.string :network
             t.string :day 
             t.integer :rating 
