7823b79dc533519492428e0a6000c73baef9bf8c
churchwerks <steven.lennis@icloud.com>
2018-01-15 17:16:45 -0600

    5 tests passing

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,2 +1,11 @@
 class Show < ActiveRecord::Base
+
+  def self.highest_rating
+    show = Show.maximum(:rating)
+  end
+
+  def self.most_popular_show
+    self.where("rating = ?", Show.maximum(:rating))
+  end
+
 end
