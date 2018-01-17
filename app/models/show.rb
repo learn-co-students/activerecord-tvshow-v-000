5a9dce5bb23f97ff29ea2f260777f32b0794bc02
Brittany Morris <brittmmendez@gmail.com>
2018-01-17 12:57:23 +0000

    Done.

---
+++ app/models/show.rb
@@ -0,0 +1,37 @@
+class Show < ActiveRecord::Base
+
+  def self.highest_rating
+  #return the highest value in the ratings column
+  Show.maximum('rating')
+  end
+
+  def self.most_popular_show
+    #return the show with the highest rating
+    Show.find_by(rating: highest_rating)
+  end
+
+  def self.lowest_rating
+    #returns the lowest value in the ratings column.
+    Show.minimum(:rating)
+  end
+
+  def self.least_popular_show
+    #returns the show with the lowest rating.
+      Show.find_by(rating: lowest_rating)
+  end
+
+  def self.ratings_sum
+    #returns the sum of all of the ratings.
+    Show.sum(:rating)
+  end
+
+  def self.popular_shows
+    #returns an array of all of the shows that have a rating greater than 5.
+    Show.where("rating > 5")
+  end
+
+  def self.shows_by_alphabetical_order
+    #returns an array of all of the shows sorted by alphabetical order according to their names
+    Show.order(:name)
+  end
+end
