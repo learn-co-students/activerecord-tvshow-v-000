ee697667d9917bd8046e4803abf978d88a14024e
funemployeddad <funemployeddad@gmail.com>
2018-02-06 16:07:08 -0700

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -5,8 +5,7 @@ class Show < ActiveRecord::Base
   end
 
   def self.most_popular_show
-    show = Show.select(:name)
-    show.highest_rating
+    self.find_by(:rating => self.highest_rating)
   end
 
   def self.lowest_rating
@@ -14,7 +13,7 @@ class Show < ActiveRecord::Base
   end
 
   def self.least_popular_show
-
+    self.find_by(:rating => self.lowest_rating)
   end
 
   def self.ratings_sum
