3e769b7ff34c083d715404028aa88e0677f050bd
Adjoa Darien <adjoa.darien@gmail.com>
2018-02-16 21:34:16 +0000

    Done.

---
+++ app/models/show.rb
@@ -0,0 +1,28 @@
+class Show < ActiveRecord::Base
+
+  def self.highest_rating
+    Show.maximum(:rating)
+  end
+
+  def self.most_popular_show
+    Show.where('rating == ?', Show.highest_rating)
+  end
+
+  def self.lowest_rating
+    Show.minimum(:rating).title
+  end
+
+  def self.least_popular_show
+    Show.where('rating == ?', Show.lowest_rating)
+  end
+
+  def self.ratings_sum
+  end
+
+  def self.popular_shows
+  end
+
+  def self.shows_by_alphabetical_order
+  end
+
+end
