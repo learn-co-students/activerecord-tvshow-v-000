c95adef254e5fd18b814bb57d057f6f9b737d769
johnwojo <jwojo13@gmail.com>
2018-03-01 13:37:48 -0500

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,2 +1,18 @@
-class Show < ActiveRecord:Base
+class Show < ActiveRecord::Base
+
+  def self.highest_rating
+    Show.maximum(:rating)
+  end
+
+  def self.most_popular_show
+    Show.find_by(rating: self.highest_rating)
+  end
+
+  def self.lowest_rating
+    Show.minimum(:rating)
+  end
+
+  def self.least_popular_show
+    Show.find_by(rating: self.lowest_rating)
+  end
 end
