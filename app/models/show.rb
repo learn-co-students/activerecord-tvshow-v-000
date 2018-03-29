d61122da8bf4359732578f67bcb5bc331c6fd45c
Sam. O <samrey2018@gmail.com>
2018-03-29 00:12:35 +0000

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -31,13 +31,12 @@ class Show < ActiveRecord::Base
 
 
   def self.popular_shows
-    binding.pry
-    self.where(rating > 5)
 
+    self.where("rating > ? ", 5)
   end
 
   def self.shows_by_alphabetical_order
-
+    self.order("name")
   end
 
 end
