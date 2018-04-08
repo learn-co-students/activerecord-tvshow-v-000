62c1769de46ef606fde7fb121414e57e4165e7c3
Michael St. Marie <michael.landrum.st.marie@gmail.com>
2018-04-08 21:16:58 +0000

    Done.

---
+++ app/models/show.rb
@@ -0,0 +1,24 @@
+require 'pry'
+
+class Show < ActiveRecord::Base
+  def self.highest_rating
+    Show.maximum("rating")
+  end
+
+  def self.most_popular_show
+    Show.find_by(rating: Show.highest_rating)
+  end
+
+
+  def self.lowest_rating
+    Show.minimum('rating')
+  end
+
+  def self.least_popular_show
+    Show.find_by(rating: Show.lowest_rating)
+  end
+
+  def self.ratings_sum
+    Show.sum('rating')
+  end
+end
