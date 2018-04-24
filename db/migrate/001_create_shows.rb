273996c8d044e4bdadc41c94b4270c0f2bef4667
Dee Race <hey@deeraceart.com>
2018-04-24 15:49:29 +0000

    Done.

---
+++ db/migrate/001_create_shows.rb
@@ -0,0 +1,12 @@
+class Show < ActiveRecord::Migration[5.1]
+  def change
+    create_table :shows do |s|
+      s.string :name
+      s.string :network
+      s.string :day
+      s.integer :rating
+    end
+  end
+
+
+end
