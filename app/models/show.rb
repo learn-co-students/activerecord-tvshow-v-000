8af06bc65812f4583c250987445c6892171f76ef
Samantha Garcia <samantha.marie.garcia@gmail.com>
2018-05-03 19:24:52 +0000

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,2 +1,27 @@
 class Show < ActiveRecord::Base
+  def self.highest_rating
+      self.maximum(:rating)
+  end
+
+  def self.most_popular_show
+      shows = self.order(rating: :DESC)
+      shows.first
+  end
+
+  def self.lowest_rating
+      self.minimum(:rating)
+  end
+
+  def self.least_popular_show
+      shows = self.order(rating: :ASC)
+      shows.first
+  end
+
+  def self.ratings_sum
+      self.sum(:rating)
+  end
+
+  def self.popular_shows
+
+  end
 end
