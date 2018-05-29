6768183aef29e6d91bd69b7e4387608d031bc6ad
Eddie Moon <eddiemoonie@gmail.com>
2018-05-29 21:41:08 +0000

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -4,4 +4,28 @@ class Show < ActiveRecord::Base
     self.maximum(:rating)
   end
 
+  def self.most_popular_show
+    self.order("rating DESC").first
+  end
+
+  def self.lowest_rating
+    self.minimum(:rating)
+  end
+
+  def self.least_popular_show
+    self.order("rating ASC").first
+  end
+
+  def self.ratings_sum
+    self.sum(:rating)
+  end
+
+  def self.popular_shows
+    self.where("rating > 5")
+  end
+
+  def self.shows_by_alphabetical_order
+    self.order("name")
+  end
+
 end
