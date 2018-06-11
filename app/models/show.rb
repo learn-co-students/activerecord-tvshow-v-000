431348c225cf2b752aa8d921674a37d9ff69ea93
Christiana Vandermale <christianavdm@gmail.com>
2018-06-11 22:41:52 +0000

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,29 +1,29 @@
 class Show < ActiveRecord::Base
-  def highest_rating
-    Show.maximum("rating").rating
-  end
-
-  def most_popular_show
+  def self.highest_rating
     Show.maximum("rating")
   end
 
-  def lowest_rating
-    Show.minimum("rating").rating
+  def self.most_popular_show
+    Show.find_by(rating: self.highest_rating)
   end
 
-  def least_popular_show
-      Show.minimum("rating")
+  def self.lowest_rating
+    Show.minimum("rating")
   end
 
-  def ratings_sum
+  def self.least_popular_show
+    Show.find_by(rating: self.lowest_rating)
+  end
+
+  def self.ratings_sum
     Show.sum("rating")
   end
 
-  def popular_shows
-    Show.where("rating 5")
+  def self.popular_shows
+    Show.where("rating > 5")
   end
 
-  def shows_by_alphabetical_order
-    Show.order("name DESC")
+  def self.shows_by_alphabetical_order
+    Show.order(:name)
   end
 end
