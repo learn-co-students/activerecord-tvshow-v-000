6ba3e80fd3d4473dd8df51b9993dc5c23fd4c84b
Anu Khambete <anukhambete@gmail.com>
2018-06-21 21:51:31 +0000

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,2 +1,31 @@
 class Show < ActiveRecord::Base
+
+  def Show::highest_rating
+    show = Show.order(:rating).last.rating
+  end
+
+  def Show::most_popular_show
+    show = Show.order(:rating).last
+  end
+
+  def Show::lowest_rating
+    show = Show.order(:rating).first.rating
+  end
+
+  def Show::least_popular_show
+    show = Show.order(:rating).first
+  end
+
+  def Show::ratings_sum
+    sum = Show.sum(:rating)
+  end
+
+  def Show::popular_shows
+    array = Show.where("rating > ?", 5)
+  end
+
+  def Show::shows_by_alphabetical_order
+    Show.order("name ASC")
+  end
+
 end
