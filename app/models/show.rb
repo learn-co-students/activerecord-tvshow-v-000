949d1982970bbd6c86a341cc1dc3d15f8facab08
Emanuel Bierman <emanuel.bierman@gmail.com>
2018-08-01 16:06:42 +0000

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,6 +1,18 @@
 class Show < ActiveRecord::Base
   def self.highest_rating
-    self.maximum
+    self.maximum(:rating)
+  end
+
+  def self.most_popular_show
+    self.order(rating: :desc).limit(1)[0]
+  end
+
+  def self.lowest_rating
+    self.minimum(:rating)
+  end
+
+  def self.least_popular_show
+    self.order(rating: :asc).limit(1)[0]
   end
 end
 
