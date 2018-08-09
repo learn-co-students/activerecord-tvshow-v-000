788f834dde0a2c9d65519fca4e7f082faa1f6999
Nathan  Visak <nathan.visak@gmail.com>
2018-08-09 22:03:33 +0000

    Done.

---
+++ app/models/show.rb
@@ -0,0 +1,24 @@
+class Show < ActiveRecord::Base
+
+  def self.highest_rating
+    Show.maximum(:rating)
+  end
+
+  def self.most_popular_show
+    Show.find_by(highest_rating)
+  end
+
+  def self.lowest_rating
+    Show.minimum(:rating)
+  end
+
+  def self.least_popular_show
+    Show.order(:rating).first
+  end
+
+  def self.ratings_sum
+    Show.sum(:rating)
+  end 
+
+
+end
