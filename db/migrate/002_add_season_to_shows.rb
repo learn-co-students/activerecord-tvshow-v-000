45c8c5dcd629bf179acd6a5c13929b2116478b57
Brenden Thornton <thorntonbrenden@gmail.com>
2018-08-20 20:52:54 +0000

    Done.

---
+++ db/migrate/002_add_season_to_shows.rb
@@ -0,0 +1,9 @@
+# season, to the shows table. The datatype of this column is string.
+
+class AddSeasonToShows < ActiveRecord::Migration
+  
+  def change
+    add_column :shows, :season, :string 
+  end 
+  
+end 
