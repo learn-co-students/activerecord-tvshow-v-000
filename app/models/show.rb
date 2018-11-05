ad35dcc4dbc6347c21cc052d0a814e67022bd3a5
Will Schultz <will.j.schultz@gmail.com>
2018-11-05 16:27:20 +0000

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -16,15 +16,18 @@ class Show < ActiveRecord::Base
       self.where("rating = ?", self.lowest_rating).first
     end
 
+
+
+    def self.shows_by_alphabetical_order
+      self.order(:name)
+    end
+
+
     def self.ratings_sum
       self.sum(:rating)
     end
 
     def self.popular_shows
       self.where("rating >= ?", 5)
-    end
-
-    def self.shows_by_alphabetical_order
-      self.order(:name)
     end
   end
