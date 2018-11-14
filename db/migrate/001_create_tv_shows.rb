48dd55de2ab0ca9cae9ada28558b53cdc8c2da4b
Harley Rayner <harleymteden@gmail.com>
2018-11-14 03:45:12 +0000

    Done.

--- db/migrate/001_create_tv_shows.rb
+++ db/migrate/001_create_tv_shows.rb
@@ -2,8 +2,10 @@ class CreateTvShows < ActiveRecord::Migration
 
   def change
     create_table :shows do |show|
-      show.integer :season
       show.string :name
+      show.string :network
+      show.string :day
+      show.integer :rating
       show.timestamps
     end
   end
