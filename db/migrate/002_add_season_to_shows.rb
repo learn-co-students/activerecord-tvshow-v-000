76b92bd73beacc05971f43d9e5691f0c9122bee7
Marcus  = Soulo <souloinc87@gmail.com>
2019-01-21 21:35:35 +0000

    Done.

--- db/migrate/002_add_season_to_shows.rb
+++ db/migrate/002_add_season_to_shows.rb
@@ -1,5 +1,5 @@
-class AddSeasonToShows < ActiveRecord::Migration[5.1]
+class AddSeasonToShows < ActiveRecord::Migration
   def change
-    add_column :shows, :string, :seasons 
+    add_column :shows, :season, :string
   end
 end
