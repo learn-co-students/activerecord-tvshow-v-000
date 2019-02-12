4d3804661d3edda0011d6dac1f04bd063ea22f18
Sheena Bonilla <sheena.myb@gmail.com>
2019-02-12 18:10:39 +0000

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,23 +1,33 @@
+require 'pry'
+
 class Show < ActiveRecord::Base
-  def highest_rating
-  show = Show.maximum("rating")
+  def self.highest_rating
+    show = Show.maximum("rating")
   end
   
-  def most_popular_show
+  def self.most_popular_show
+    show = Show.order(rating: :desc).limit(1).take
   end
   
-  def lowest_rating
+  def self.lowest_rating
+    show = Show.minimum("rating")
   end
   
-  def least_popular_show
+  def self.least_popular_show
+    show = Show.order(rating: :asc).limit(1).take
   end
   
-  def ratings_sum
+  def self.ratings_sum
+    show = Show.sum("rating")
   end
   
-  def popular_shows
+  def self.popular_shows
+    binding.pry
+    show = Show.order(rating: :asc).limit(1)
   end
   
-  def shows_by_alphabetical_order
-  end
+  # def shows_by_alphabetical_order
+  # end
 end
+
+  
