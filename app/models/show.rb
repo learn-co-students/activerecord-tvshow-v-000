2215d830c1c59959b4f34f586ca94f1776c6eeb7
ktochow1 <ktochow1@binghamton.edu>
2019-03-18 20:55:50 +0000

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,22 +1,34 @@
-
-
-# Methods: 
-# - highest_rating - return highest value of ratings column 
-# - most popular show - highest rating = helper method 
-# - lowest rating (lowest value in ratings)
-# - least popular show (lowest rating)
-# - ratings sum 
-# - popular shws (returns array of all shows > 5)
-#   use where 
-#   -shows by alphabetical order 
-
 class Show < ActiveRecord::Base 
 
   def self.highest_rating
     self.maximum(:rating) 
   end
   
+  def self.most_popular_show
+    self.where("rating = ?", self.highest_rating).first
+  end
+  #look into solution/syntax/logic 
   
+  def self.lowest_rating
+    self.minimum(:rating)
+  end 
+  
+  def self.least_popular_show 
+    self.where("rating = ?", self.lowest_rating).first
+  end
+  
+  def self.ratings_sum 
+    self.sum(:rating)
+  end
     
+  def self.popular_shows
+    show = Show.where("rating >= ?", 5)
+  end 
+  #write/understand method 
   
+  
+  def self.shows_by_alphabetical_order 
+    Show.order(name: :asc)
+  end
+  #write/understand method/syz
 end
