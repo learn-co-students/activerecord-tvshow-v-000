38d5d05e894daa976d711969cffd82ff7a5ed874
Mariel Hoepelman <marielhoepelman@gmail.com>
2019-03-22 19:44:44 +0000

    WIP

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,4 +1,13 @@
 # In this file, you will define a `Show` class that inherits from `ActiveRecord::Base`.
-
 class Show < ActiveRecord::Base
+
+  def self.highest_rating
+    Show.maximum(:rating)
+  end
+
+  def self.most_popular_show
+    Show.highest_rating(:name)
+
+  end
+
 end
--- db/migrate/002_add_season_to_shows.rb
+++ db/migrate/002_add_season_to_shows.rb
@@ -1,6 +1,6 @@
 #write a migration to add a column, `season`, to the `shows` table. The datatype of this column is string.
 
-class AddSeasonToShows < ActiveRecord::Migration[4.2]
+class AddSeasonToShows < ActiveRecord::Migration
   def change
     add_column :shows, :season, :string
   end
