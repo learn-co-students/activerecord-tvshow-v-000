1a1919b694acca4785fbffb21ee07ce436b03646
Mariel Hoepelman <marielhoepelman@gmail.com>
2019-03-22 20:36:39 +0000

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -6,8 +6,26 @@ class Show < ActiveRecord::Base
   end
 
   def self.most_popular_show
-    Show.highest_rating(:name)
-
+    Show.find_by(rating:highest_rating)
   end
 
+  def self.lowest_rating
+    Show.minimum(:rating)
+  end
+
+  def self.least_popular_show
+    Show.find_by(rating:lowest_rating)
+  end
+
+  def self.ratings_sum
+    Show.sum(:rating)
+  end
+
+  def self.popular_shows
+    Show.where("rating > ?", 5)
+  end
+
+  def self.shows_by_alphabetical_order
+    Show.all.order(name: :asc)
+  end
 end
