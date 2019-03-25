bcbd2f40d5884e11f46c6622741c2de652ab3305
Grace Choi <oiograceoio@gmail.com>
2019-03-25 18:45:23 +0000

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,5 +1,9 @@
 class Show < ActiveRecord::Base
   def highest_rating
-    self.minimum(:rating)
+    Show.minimum(:rating)
+  end
+
+  def most_popular_show
+    Show.find_by(rating: highest_rating)
   end
 end
