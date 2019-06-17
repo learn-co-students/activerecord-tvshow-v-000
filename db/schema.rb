b85a0c5d78022eb8a2254d734c0fd3f72b969f21
David Charles <ratlover03@gmail.com>
2019-06-17 22:15:57 +0000

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,10 @@
 ActiveRecord::Schema.define(version: 2) do
 
   create_table "shows", force: :cascade do |t|
+    t.string "name"
+    t.string "day"
+    t.string "network"
+    t.integer "rating"
     t.string "season"
   end
 
