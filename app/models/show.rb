96d1d73dbaf446b33d3c0f691a6014b60e61321e
Kari O'Neal <triadfirewood@gmail.com>
2019-06-19 12:06:47 +0000

    Done.

---
+++ app/models/show.rb
@@ -0,0 +1,35 @@
+require'pry'
+class Show < ActiveRecord::Base 
+  
+  def highest_rating
+    binding.pry
+    Show.maximum("rating")
+    
+  end 
+  
+  def most_popular_show 
+    
+  end
+  
+  
+  def lowest_rating
+    Show.minimum("rating")
+  end 
+  
+  def least_popular_show
+    
+  end 
+  
+  def ratings_sum
+    
+  end 
+  
+  def popular_shows
+    
+  end 
+  
+  def shows_by_alphabetical_order 
+    
+  end  
+  
+end
