4c99f994ed40a20e06738a24a871296727b6c74a
Karen Flores <karen.ag.flores@gmail.com>
2019-07-02 20:19:17 +0000

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -6,6 +6,7 @@ class Show < ActiveRecord::Base
   def self.most_popular_show
     show = Show.maximum(:rating)
     most_pop = Show.find_by(rating: show)
+    #self.where("rating = ?", self.highest_rating).first
   end
 
   def self.lowest_rating
