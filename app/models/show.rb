505907f8afe3e01d4e7f83b4d6ae176cb80ee05f
J Fern <jfern20@gmail.com>
2019-10-21 22:23:39 +0000

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,4 +1,20 @@
 class Show < ActiveRecord::Base 
   
   
+  def self.highest_rating
+    Show.maximum(:rating)   #highest value in ratings column
+  end 
+  
+  def self.most_popular_show
+    Show.where(rating: highest_rating).first
+  end 
+
+  
+  def self.lowest_rating
+    Show.minimum(:rating)
+  end 
+  
+  
+  
+  
 end 
